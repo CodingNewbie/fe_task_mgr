276b3dbc204927567c3c17868d00a99b7dc26152
Adam <adamrayschmidt@gmail.com>
2024-04-27 10:59:41 -0700

    Add edit task functionality.

--- app/routes.py
+++ app/routes.py
@@ -39,3 +39,30 @@ def detail(pk):
         render_template("error.html", err=response.status_code),
         response.status_code
     )
+
+@app.get("/tasks/edit/<int:pk>")
+def edit_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        task = response.json().get("task")
+        return render_template("edit.html", task=task)
+    return (
+        render_template("error.html", err=response.status_code),
+        response.status_code
+    )
+
+@app.post("/tasks/edit/<int:pk>")
+def submit_task_update(pk):
+    print("Received data:", request.form)
+    url = "%s/%s" % (BACKEND_URL, pk)
+    task_data = request.form
+    print("Sending data to backend:", task_data)
+    response = requests.put(url, json=task_data)
+    print("Backend response status:", response.status_code)
+    if response.status_code == 204:
+        return render_template("success.html", msg="Task updated successfully.")
+    return (
+        render_template("error.html", err=response.status_code),
+        response.status_code
+    )
